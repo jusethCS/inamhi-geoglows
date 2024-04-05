0a4730c8ded880038428700707b08101121bc2e0
Juseth Chancay <juseth.chancay@gmail.com>
2024-04-05 00:55:10 -0500

    Added authentication

--- frontend/src/app/auth/components/login/login.component.ts
+++ frontend/src/app/auth/components/login/login.component.ts
@@ -1,51 +1,101 @@
-import {Component} from '@angular/core';
-import {Router} from '@angular/router';
-import {MatSelectModule} from '@angular/material/select';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
-import {FormControl, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {merge} from 'rxjs';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
-    MatSelectModule,
     MatInputModule,
-    MatFormFieldModule,
     MatIconModule,
-    MatButtonModule,
-    FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatButtonModule
   ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
+
+
 export class LoginComponent {
+
+  // Form variables and error messages
   email = new FormControl('', [Validators.required, Validators.email]);
-  errorMessage = '';
+  errorMessageEmail = '';
+
+  password = new FormControl('', [Validators.required]);
+  errorMessagePassword = '';
+
+  // State varible to hide or show password
   hide = true;
 
-  constructor(private router: Router) {
-    merge(this.email.statusChanges, this.email.valueChanges)
+
+  // -------------------------------------------------------------------- //
+  //          CONSTRUCTOR: Validate the state of form variables           //
+  // -------------------------------------------------------------------- //
+  constructor(private router: Router, private authService: AuthService) {
+    this.email.valueChanges
       .pipe(takeUntilDestroyed())
-      .subscribe(() => this.updateErrorMessage());
+      .subscribe(() => this.updateErrorMessageEmail());
+
+    this.password.valueChanges
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.updateErrorMessagePassword());
   }
 
-  updateErrorMessage() {
-    if (this.email.hasError('required')) {
-      this.errorMessage = 'Debe ingresar un correo';
-    } else if (this.email.hasError('email')) {
-      this.errorMessage = 'Correo no válido';
-    } else {
-      this.errorMessage = '';
-    }
+
+  // -------------------------------------------------------------------- //
+  //           METHODS: Update status and erros in form values            //
+  // -------------------------------------------------------------------- //
+  updateErrorMessageEmail() {
+    this.errorMessageEmail = this.email.hasError('required')
+      ? 'Debe ingresar un correo'
+      : this.email.hasError('email')
+        ? 'Correo no válido'
+        : '';
+  }
+  updateErrorMessagePassword() {
+    this.errorMessagePassword = this.password.hasError('required')
+      ? 'Debe ingresar una contraseña'
+      : '';
   }
 
+
+  // -------------------------------------------------------------------- //
+  //           METHOD: Show the register form (Create new user)           //
+  // -------------------------------------------------------------------- //
   redirectToRegister() {
     this.router.navigateByUrl('/register');
   }
+
+
+  // -------------------------------------------------------------------- //
+  //            METHOD: User authentication and JWT generation            //
+  // -------------------------------------------------------------------- //
+  login() {
+    const email = this.email.value;
+    const password = this.password.value;
+
+    if (email && password) {
+      this.authService.get_token(email, password).subscribe({
+        next: (token) => {
+          this.authService.login(token.token);
+        },
+        error: (err) => {
+          console.error("Error during login:", err);
+        },
+        complete: () =>{
+          console.log("Login successfully");
+          this.router.navigateByUrl('/apps');
+        }
+      });
+    }
+  }
+
+
 }
