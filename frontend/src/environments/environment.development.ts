a77700c8b9662971b6d73da41986917f1fa2b5ce
Juseth Chancay <juseth.chancay@gmail.com>
2024-04-17 00:44:36 -0500

    Added environmental variables

---
+++ frontend/src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  urlHost: "http://localhost:4200/",
+  urlAPI: "http://localhost:8000/"
+};
---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  urlHost: "http://ec2-3-211-227-44.compute-1.amazonaws.com/",
+  urlAPI: "http://ec2-3-211-227-44.compute-1.amazonaws.com/api/"
+};
