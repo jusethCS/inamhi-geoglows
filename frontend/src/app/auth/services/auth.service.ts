291fac34cf46122ffa19752aa8707301bcc62409
Juseth Chancay <juseth.chancay@gmail.com>
2024-04-23 15:03:17 -0500

    Replace enviroment file

--- frontend/src/app/auth/services/auth.service.ts
+++ frontend/src/app/auth/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from '../../../environments/environment.development';
+import { environment } from '../../../environments/environment';
 
 
 import { Inject } from '@angular/core';
