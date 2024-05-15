20742bec1600a0ae41e01a6ff2ec2f7091216410
Juseth Chancay <juseth.chancay@gmail.com>
2024-05-15 15:37:04 -0500

    Created a new project version

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,18 +2,7 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient, withFetch } from '@angular/common/http';
-
-import { JWT_OPTIONS, JwtHelperService } from '@auth0/angular-jwt';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(routes),
-    provideClientHydration(),
-    provideAnimationsAsync(),
-    provideHttpClient(withFetch()),
-    JwtHelperService, { provide: JWT_OPTIONS, useValue: JWT_OPTIONS },
-  ]
+  providers: [provideRouter(routes)]
 };
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,3 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [
-  { path:'', loadChildren: () => import("./views/home/home.routes").then(m => m.HOME_ROUTES) },
-  { path:'apps', loadChildren: () => import("./views/dashboard/dashboard.routes").then(m => m.DASHBOARD_ROUTES)}
-
-];
+export const routes: Routes = [];
