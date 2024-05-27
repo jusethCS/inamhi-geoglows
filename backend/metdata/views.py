239916db238ba7de2d7a3aed1ea88371d7d6f174
Juseth Chancay <juseth.chancay@gmail.com>
2024-05-27 11:29:46 -0500

    Print error

--- backend/metdata/views.py
+++ backend/metdata/views.py
@@ -38,29 +38,33 @@ def fetch_raster_value(date, workspace, gdf):
     return {'date': dd, 'value': value}
 
 def get_metdata(request):
-    product = request.GET.get('product')
-    temporality = request.GET.get('temp')
-    start = request.GET.get('start')
-    end = request.GET.get('end')
-    code = request.GET.get('code')
+    try:
+        product = request.GET.get('product')
+        temporality = request.GET.get('temp')
+        start = request.GET.get('start')
+        end = request.GET.get('end')
+        code = request.GET.get('code')
 
-    if code.endswith("00"):
-        url = f"{GEOSERVER}/ecuador-limits/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=ecuador-limits%3Aprovincias&maxFeatures=50&outputFormat=application%2Fjson&CQL_FILTER=DPA_CANTON={code}"
-    else:
-        url = f"{GEOSERVER}/ecuador-limits/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=ecuador-limits%3Acantones&maxFeatures=50&outputFormat=application%2Fjson&CQL_FILTER=DPA_CANTON={code}"
+        if code.endswith("00"):
+            url = f"{GEOSERVER}/ecuador-limits/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=ecuador-limits%3Aprovincias&maxFeatures=50&outputFormat=application%2Fjson&CQL_FILTER=DPA_CANTON={code}"
+        else:
+            url = f"{GEOSERVER}/ecuador-limits/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=ecuador-limits%3Acantones&maxFeatures=50&outputFormat=application%2Fjson&CQL_FILTER=DPA_CANTON={code}"
 
-    area = requests.get(url).json()
-    gdf = gpd.GeoDataFrame.from_features(area["features"])
+        area = requests.get(url).json()
+        gdf = gpd.GeoDataFrame.from_features(area["features"])
 
-    workspace = f"{product}-{temporality}"
-    if temporality == "daily":
-        dates = pd.date_range(start=start, end=end, freq='D')
-    elif temporality == "monthly":
-        dates = pd.date_range(start=start, end=end, freq='MS')
-    elif temporality == "annual":
-        dates = pd.date_range(start=start, end=end, freq='YS')
+        workspace = f"{product}-{temporality}"
+        if temporality == "daily":
+            dates = pd.date_range(start=start, end=end, freq='D')
+        elif temporality == "monthly":
+            dates = pd.date_range(start=start, end=end, freq='MS')
+        elif temporality == "annual":
+            dates = pd.date_range(start=start, end=end, freq='YS')
 
-    with ThreadPoolExecutor(max_workers=2) as executor:
-        results = list(executor.map(lambda date: fetch_raster_value(date, workspace, gdf), dates))
+        with ThreadPoolExecutor(max_workers=2) as executor:
+            results = list(executor.map(lambda date: fetch_raster_value(date, workspace, gdf), dates))
 
-    return JsonResponse(results, safe=False)
+        return JsonResponse(results, safe=False)
+
+    except ValueError as e:
+        return JsonResponse({"error": e})
