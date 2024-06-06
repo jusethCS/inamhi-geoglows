50d84e5dee4baf0bcc87d6c55f54e29068c582d0
Juseth Chancay <juseth.chancay@gmail.com>
2024-06-06 15:43:42 -0500

    rename sh routines

--- taskfiles/meteosat/contrab.sh
+++
@@ -1,3 +0,0 @@
-0 0 28 * * /home/ubuntu/miniconda3/envs/meteosat/bin/python /home/ubuntu/inamhi-geoglows/taskfiles/meteosat/chirps.py
-
-#0 8 * * * /home/ubuntu/miniconda3/envs/geoglows_ecuador/bin/python /home/ubuntu/tethys_apps_ecuador/geoglows_database_ecuador/r_forecast_analysis.py > /home/ubuntu/tethys_apps_ecuador/forecast.log 2>&1
---
+++ taskfiles/meteosat/r_monthly.sh
@@ -0,0 +1,2 @@
+/home/ubuntu/miniconda3/envs/meteosat/bin/python /home/ubuntu/inamhi-geoglows/taskfiles/meteosat/chirps.py
+/home/ubuntu/miniconda3/envs/meteosat/bin/python /home/ubuntu/inamhi-geoglows/taskfiles/meteosat/imerg-final.py
