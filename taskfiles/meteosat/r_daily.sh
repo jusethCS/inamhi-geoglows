450dfb8a8c95f0c7204f3e53901f0b87c0c33615
Juseth Chancay <juseth.chancay@gmail.com>
2024-06-08 10:50:38 -0500

    Removed logs daily

--- taskfiles/meteosat/r_daily.sh
+++ taskfiles/meteosat/r_daily.sh
@@ -1,3 +1,4 @@
+rm -rf /home/ubuntu/logs/*
 /home/ubuntu/miniconda3/envs/meteosat/bin/python /home/ubuntu/inamhi-geoglows/taskfiles/meteosat/persiann.py
 /home/ubuntu/miniconda3/envs/meteosat/bin/python /home/ubuntu/inamhi-geoglows/taskfiles/meteosat/persiann-ccs.py
 /home/ubuntu/miniconda3/envs/meteosat/bin/python /home/ubuntu/inamhi-geoglows/taskfiles/meteosat/persiann-pdir.py
