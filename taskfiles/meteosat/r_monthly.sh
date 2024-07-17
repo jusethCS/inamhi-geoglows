a019d4115ff1f384f164717fc7a951e72c9ce1ec
Juseth Chancay <juseth.chancay@gmail.com>
2024-07-16 19:50:18 -0500

    Modified structure

--- taskfiles/meteosat/r_monthly.sh
+++
@@ -1,2 +0,0 @@
-/home/ubuntu/miniconda3/envs/meteosat/bin/python /home/ubuntu/inamhi-geoglows/taskfiles/meteosat/chirps.py
-/home/ubuntu/miniconda3/envs/meteosat/bin/python /home/ubuntu/inamhi-geoglows/taskfiles/meteosat/imerg-final.py
