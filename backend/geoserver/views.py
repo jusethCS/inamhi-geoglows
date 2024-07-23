26d04a935c12708725d0d3cf8ef316951a82de8d
Juseth Chancay <juseth.chancay@gmail.com>
2024-07-22 22:52:12 -0500

    Fixed url

--- backend/geoserver/views.py
+++ backend/geoserver/views.py
@@ -4,8 +4,8 @@ from django.http import HttpResponse
 ENDPOINT = "/usr/share/geoserver/data_dir/data" 
 
 def download(request):
-    workspace = request.GET.get('workspace')
-    layer = request.GET.get('layer')
+    workspace = "fireforest"#request.GET.get('workspace')
+    layer = "daily_precipitation"#request.GET.get('layer')
     file_path = f'{ENDPOINT}/{workspace}/{layer}/{layer}.geotiff'
     try:
         with rasterio.open(file_path) as dataset:
