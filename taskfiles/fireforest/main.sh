4e514d10d4358cb724baf22356cd0a61937f3f6f
Juseth Chancay <juseth.chancay@gmail.com>
2024-08-22 07:44:17 -0500

    Call geoglows enviroment

--- taskfiles/fireforest/main.sh
+++ taskfiles/fireforest/main.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+env > /home/ubuntu/cron_env.log
+source /home/ubuntu/miniconda3/etc/profile.d/conda.sh
+conda activate geoglows
 /home/ubuntu/miniconda3/envs/geoglows/bin/python /home/ubuntu/inamhi-geoglows/taskfiles/fireforest/daily_pacum.py
 /home/ubuntu/miniconda3/envs/geoglows/bin/python /home/ubuntu/inamhi-geoglows/taskfiles/fireforest/soil_moisture.py
 /home/ubuntu/miniconda3/envs/geoglows/bin/python /home/ubuntu/inamhi-geoglows/taskfiles/fireforest/report.py
