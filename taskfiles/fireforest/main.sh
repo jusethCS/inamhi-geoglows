81fc53698cba0e1a528722ba1d2d46d807e31303
Juseth Chancay <juseth.chancay@gmail.com>
2024-08-22 23:25:51 -0500

    test

--- taskfiles/fireforest/main.sh
+++ taskfiles/fireforest/main.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 source /home/ubuntu/miniconda3/etc/profile.d/conda.sh
 conda activate geoglows
+#python /home/ubuntu/inamhi-geoglows/taskfiles/fireforest/daily_pacum.py
+#python /home/ubuntu/inamhi-geoglows/taskfiles/fireforest/soil_moisture.py
+#python /home/ubuntu/inamhi-geoglows/taskfiles/fireforest/report.py
 python /home/ubuntu/inamhi-geoglows/taskfiles/sgr/main.py
 
 #/home/ubuntu/miniconda3/envs/geoglows/bin/python /home/ubuntu/inamhi-geoglows/taskfiles/fireforest/daily_pacum.py
