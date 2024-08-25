4dd54eed588792eb11c817229ac86e6d8a39f66f
Juseth Chancay <juseth.chancay@gmail.com>
2024-08-24 23:16:07 -0500

    Added report task

--- taskfiles/celec/main_paute.py
+++ taskfiles/celec/main_paute.py
@@ -854,6 +854,201 @@ def corrected_geoglows_plot(comid, obs_path, conn, outpath, titulo):
 
 
 
+###############################################################################
+def header(canvas, doc, content):
+    canvas.saveState()
+    w, h = content.wrap(doc.width, doc.topMargin)
+    content.drawOn(canvas, doc.leftMargin, doc.height + doc.bottomMargin + doc.topMargin - h)
+    canvas.restoreState()
+
+def footer(canvas, doc, content):
+    canvas.saveState()
+    w, h = content.wrap(doc.width, doc.bottomMargin)
+    content.drawOn(canvas, doc.leftMargin, h)
+    canvas.restoreState()
+
+def header_and_footer(canvas, doc, header_content, footer_content):
+    header(canvas, doc, header_content)
+    footer(canvas, doc, footer_content)
+
+def get_datetime():
+    # Obtener la fecha y hora actual
+    now = datetime.now() + timedelta(hours=-5)
+    # Mapeo de nombres de meses en inglés a español
+    meses_ingles_a_espanol = {
+        "January": "enero",
+        "February": "febrero",
+        "March": "marzo",
+        "April": "abril",
+        "May": "mayo",
+        "June": "junio",
+        "July": "julio",
+        "August": "agosto",
+        "September": "septiembre",
+        "October": "octubre",
+        "November": "noviembre",
+        "December": "diciembre"
+    }
+    # Formatear la fecha y hora de emisión
+    emision = "<b>Hora y fecha de emision:</b> " + now.strftime("%d de %B del %Y, %H:%M")
+    for mes_ingles, mes_espanol in meses_ingles_a_espanol.items():
+        emision = emision.replace(mes_ingles, mes_espanol)
+    #
+    # Formatear dia anterior
+    anterior = (now + timedelta(days=-1)).strftime("%d de %B del %Y")
+    for mes_ingles, mes_espanol in meses_ingles_a_espanol.items():
+        anterior = anterior.replace(mes_ingles, mes_espanol)
+    #
+    # Calcular la vigencia para 24 horas
+    inicio_vigencia = now.strftime("desde 07:00 del %d de %B")
+    fin_vigencia = (now + timedelta(days=1)).strftime("hasta las 07:00 del %d de %B del %Y")
+    for mes_ingles, mes_espanol in meses_ingles_a_espanol.items():
+        inicio_vigencia = inicio_vigencia.replace(mes_ingles, mes_espanol)
+        fin_vigencia = fin_vigencia.replace(mes_ingles, mes_espanol)
+    #
+    # Formatear la vigencia
+    vigencia = f"<b>Vigencia:</b> {inicio_vigencia} {fin_vigencia}"
+    return(emision, vigencia, anterior)
+
+
+def agregar_tabla(datos):
+    datos_tabla = [datos.columns.tolist()] + datos.values.tolist()
+    tabla = Table(datos_tabla)
+    tabla.setStyle(TableStyle([('BACKGROUND', (0,0), (-1,0), colors.grey),
+                               ('TEXTCOLOR', (0,0), (-1,0), colors.whitesmoke),
+                               ('ALIGN', (0,0), (-1,-1), 'CENTER'),
+                               ('FONTSIZE', (0, 0), (-1, -1), 7),
+                               ('BOTTOMPADDING', (0,0), (-1,0), 2),
+                               ('BACKGROUND', (0,1), (-1,-1), colors.white),
+                               ('GRID', (0,0), (-1,-1), 0.5, colors.black)]))
+    return(tabla)
+
+
+def report(filename, pacum, pacum_table, paute_table, cuenca_table, gualaceo_table, mazar_table, juval_table, palmira_table):
+    # Vars
+    header_path = "report_header.png"
+    footer_path = "report_footer.png"
+    titulo = "<b>Boletín Hidrometeorológico Especial Paute</b>"
+    emision, vigencia, anterior = get_datetime()
+    parrafo_1 = "La <b>DIRECCIÓN DE PRONÓSTICOS Y ALERTAS HIDROMETEOROLÓGICAS DEL INAMHI</b>, basándose en la información obtenida de la plataforma INAMHI GEOGLOWS emite el siguiente boletín de vigilancia y predicción de condiciones hidrometeorológicas:"
+    subtitulo_1 = "<b>Precipitación acumulada diaria</b>"
+    parrafo_2 = f"De acuerdo con los datos del hidroestimador satelital GPM IMERG Early Run, la precipitación media registrada en la Cuenca del río Paute durante el {anterior}, fue de {pacum} mm. A continuación se presenta un desglose de la precipitación media por subcuencas."
+    #
+    # Configurar estilos
+    estilos = getSampleStyleSheet()
+    #
+    estilo_titulo = ParagraphStyle(
+        name = 'Title',
+        fontSize = 18,
+        textColor = colors.Color(31/255, 73/255, 125/255),
+        alignment = TA_CENTER)
+    #
+    estilo_subtitulo = ParagraphStyle(
+        name = 'Subtitle',
+        fontSize = 14,
+        textColor = colors.Color(31/255, 73/255, 125/255),
+        alignment = TA_CENTER)
+    #
+    estilo_fecha = ParagraphStyle(
+        name = 'Dates',
+        fontSize = 10,
+        alignment = TA_CENTER,
+        spaceBefore = 5,
+        spaceAfter = 5)
+    #
+    estilo_parrafo = ParagraphStyle(
+        name = 'P01',
+        fontSize = 10,
+        alignment = TA_CENTER,
+        spaceBefore = 5,
+        spaceAfter = 5,
+        leading = 16)
+    #
+    estilo_parrafo2 = ParagraphStyle(
+        name = 'P02',
+        fontSize = 10,
+        alignment = TA_JUSTIFY,
+        spaceBefore = 5,
+        spaceAfter = 5,
+        leading = 16)
+    #
+    # Crear el documento PDF
+    doc = SimpleDocTemplate(filename, pagesize=letter)
+    #
+    # Definir el encabezado y pie de pagina
+    header_content = Image(header_path, width=doc.width, height=2.5*cm)
+    footer_content = Image(footer_path, width=doc.width, height=1.5*cm)
+    #
+    # Agregar elementos al contenido del PDF
+    elementos = [
+        Paragraph(titulo, estilo_titulo),
+        Spacer(1, 14),
+        Paragraph(emision, estilo_fecha),
+        Paragraph(vigencia, estilo_fecha),
+        Spacer(1, 14),
+        Paragraph(parrafo_1, estilo_parrafo),
+        Spacer(1, 14),
+        Paragraph(subtitulo_1, estilo_subtitulo),
+        Spacer(1, 10),
+        Image("pacum.png", width=doc.width, height=8*cm),
+        Image("pacum24.png", width=12*cm, height=1.2*cm),
+        Spacer(1, 12),
+        Paragraph(parrafo_2, estilo_parrafo2),
+        Spacer(1, 5),
+        agregar_tabla(pacum_table),
+        PageBreak(),
+        ##
+        Paragraph("<b>Pronóstico corregido de caudales</b>", estilo_subtitulo),
+        Spacer(1, 16),
+        Paragraph("<b>Rio Paute (en Paute) - Estación H0894</b>", estilo_parrafo),
+        Image("paute.png", width=doc.width, height=6*cm),
+        Spacer(1, 16),
+        agregar_tabla(paute_table),
+        ##
+        PageBreak(),
+        Paragraph("<b>Rio Cuenca (Pte. Europa)</b>", estilo_parrafo),
+        Image("cuenca.png", width=doc.width, height=6*cm),
+        Spacer(1, 16),
+        agregar_tabla(cuenca_table),
+        ##
+        PageBreak(),
+        Paragraph("<b>Rio Gualaceo (Sta. Bárbara)</b>", estilo_parrafo),
+        Image("gualaceo.png", width=doc.width, height=6*cm),
+        Spacer(1, 16),
+        agregar_tabla(gualaceo_table),
+        ##
+        PageBreak(),
+        Paragraph("<b>Pronóstico de caudales</b>", estilo_subtitulo),
+        Spacer(1, 16),
+        Paragraph("<b>Rio Mazar</b>", estilo_parrafo),
+        Image("mazar.png", width=doc.width, height=6*cm),
+        Spacer(1, 16),
+        agregar_tabla(mazar_table),
+        ##
+        PageBreak(),
+        Paragraph("<b>Rio Juval</b>", estilo_parrafo),
+        Image("juval.png", width=doc.width, height=6*cm),
+        Spacer(1, 16),
+        agregar_tabla(juval_table),
+        ##
+        PageBreak(),
+        Paragraph("<b>Rio Palmira</b>", estilo_parrafo),
+        Image("palmira.png", width=doc.width, height=6*cm),
+        Spacer(1, 16),
+        agregar_tabla(palmira_table),
+        Spacer(1, 90),
+        Paragraph("<b>Elaborado por:</b> DPA INAMHI en colaboración con SERVIR AMAZONIA", estilo_parrafo2),
+    ]
+    #
+    # Contruir el pdf
+    doc.build(
+        elementos, 
+        onFirstPage=partial(header_and_footer, header_content=header_content, footer_content=footer_content), 
+        onLaterPages=partial(header_and_footer, header_content=header_content, footer_content=footer_content))
+
+
+
+
 
 ###############################################################################
 #                                MAIN ROUTINE                                 #
@@ -923,3 +1118,15 @@ juval_table = geoglows_plot(9032294, conn, "juval.png", "Pronóstico de caudales
 palmira_table = geoglows_plot(9032324, conn, "palmira.png", "Pronóstico de caudales del Rio Palmira")
 
 
+# Generate report
+filename = dt.datetime.now().strftime('boletin-paute_%Y-%m-%d.pdf')
+report(
+    filename, 
+    pacum=pacum_basin.pacum[0], 
+    pacum_table=pacum_subbasins,
+    paute_table=paute_table,
+    cuenca_table = cuenca_table,
+    gualaceo_table = gualaceo_table,
+    mazar_table = mazar_table,
+    juval_table = juval_table,
+    palmira_table = palmira_table)
