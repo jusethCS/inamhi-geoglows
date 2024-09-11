52711652566c4f8b35f93ff7600200c5d2e3bb34
Juseth Chancay <juseth.chancay@gmail.com>
2024-09-11 18:50:27 -0500

    Fixed real-time hotspots from goes

--- backend/geoglows/controllers/fireforest.py
+++ backend/geoglows/controllers/fireforest.py
@@ -67,22 +67,6 @@ def get_goes_hotspots():
     now = dt.datetime.now()
     start = (now - dt.timedelta(hours=2)).strftime("%Y-%m-%d %H:%M:00")
     con = db.connect()
-    sql = f"select * from goes_hotspots where datetime>'{start}'"
-    query = pd.read_sql(sql, con)
-    con.close()
-    query['datetime'] = query['datetime'].apply(lambda x:x.strftime("%Y-%m-%d %H:%M:00"))
-    query['geometry'] = query.apply(lambda row: Point(row['longitude'], row['latitude']), axis=1)
-    gdf = gpd.GeoDataFrame(query, geometry='geometry')
-    geojson_dict = gdf.__geo_interface__
-    return(geojson_dict)
-
-
-
-
-def get_goes_hotspots():
-    now = dt.datetime.now()
-    start = (now - dt.timedelta(hours=2)).strftime("%Y-%m-%d %H:%M:00")
-    con = db.connect()
     sql = f"select distinct * from goes_hotspots where datetime > '{start}' order by datetime"
     query = pd.read_sql(sql, con)
     con.close()
