b53ef5f0fd8b76d16ca94e409a5d1d3bb8bf6739
Juseth Chancay <juseth.chancay@gmail.com>
2024-09-29 18:40:20 -0500

    Parse to list data

--- backend/app_national_water_level_forecast/controllers.py
+++ backend/app_national_water_level_forecast/controllers.py
@@ -459,14 +459,17 @@ def historical_plot(cor, obs, code, name):
     dates = cor.index.tolist()
     startdate = dates[0]
     enddate = dates[-1]
+    
+    # Convert ndarray to lists
     corrected_data = {
         'x_datetime': cor.index.tolist(),
-        'y_flow': cor.values.flatten(),
+        'y_flow': cor.values.flatten().tolist(),  # Convert to list
     }
     observed_data = {
         'x_datetime': obs.index.tolist(),
-        'y_flow': obs.values.flatten(),
+        'y_flow': obs.values.flatten().tolist(),  # Convert to list
     }
+    
     scatter_plots = [
         go.Scatter(
             name='Simulación histórica corregida', 
@@ -477,6 +480,7 @@ def historical_plot(cor, obs, code, name):
             x=observed_data['x_datetime'], 
             y=observed_data['y_flow'])
     ]
+    
     layout = go.Layout(
         title=f"Simulación histórica <br>{str(code).upper()} - {name}",
         yaxis={
@@ -488,13 +492,14 @@ def historical_plot(cor, obs, code, name):
             'hoverformat': '%b %d %Y', 
             'tickformat': '%Y'},
     )
+    
     figure = go.Figure(scatter_plots, layout=layout)
     figure.update_layout(template='simple_white')
     figure.update_yaxes(linecolor='gray', mirror=True, showline=True) 
-    figure.update_xaxes(linecolor='gray', mirror=True, showline=True)
+    figure.update_xaxes(linecolor='gray', mirror=True, showline=True)    
     figure_dict = figure.to_dict()
     return figure_dict
-    
+
 
 
 
